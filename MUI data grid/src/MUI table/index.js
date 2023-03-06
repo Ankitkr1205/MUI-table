e42b6155424ebdbdaf0dd44d6dbc3f525f8254cd
Ankit Chaudhary <chaudharyankit573nets@gmail.com>
2023-03-06 15:56:11 +0530

    lookups

--- MUI data grid/src/MUI table/index.js
+++ MUI data grid/src/MUI table/index.js
@@ -5,35 +5,49 @@ import axios from 'axios';
 const TableListing=()=>{
 
     const [data,setData]=useState([]);
+    const [statusData,setStatusData]=useState({});
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
-            console.log('response',res);
+        axios.get('https://jsonplaceholder.typicode.com/users').then((res)=>{
+            console.log('responseAPI',res);
             console.log('response',res.data);
             setData(res.data);
         })
     },[])
     console.log('setData',data)
 
+    const oldStatus=[
+        {id:0,title:"Not Avilable"},
+        {id:1,title:"Avilable"},
+    ]
+
+    useEffect(()=>{
+        const newStatus={};
+        oldStatus.map(statusData=>newStatus[statusData.id]=statusData.title)  
+        setStatusData(newStatus)
+        console.log('status',newStatus);
+    },[])
+
     const dataStatic=[
-        {
-            name:"DHONI",role:'CSK'
-        },
-        {
-            name:"Kohli",role:'RCB'
-        }
+        {name:"DHONI",role:'CSK',country:"India",status:0,catagory:7,venue:3},
+        { name:"Kohli",role:'RCB',country:"India",status:1,catagory:3,venue:2},
+        { name:"Smith",role:'RR',country:"Austrlia",status:1,catagory:5,venue:2},
+        { name:"Archer",role:'MI',country:"England",status:0,catagory:4,venue:3},
         
     ]
 
+    const columnsStatic=[  
+        {title:"name",field:"name"},
+        {title:"role",field:"role" },
+        {title:"country",field:"country"},
+        {title:"Venue",field:"venue",lookup:{3:"Gujrat",2:"Delhi"}},
+        {title:"catagorycatagory",field:"venue",lookup:{7:"Allrounder",3:"Batsman",5:"Bowler",4:"keeper"}},
+        {title:"status",field:"status",lookup:statusData},
+        ]
+
     const columns=[  
-        {
-            title:"id",field:"id"
-        },
-        {
-            title:"body",field:"body"
-        },
-        {
-            title:"title",field:"title"
-        },
+        { title:"id",field:"id"},
+        { title:"body",field:"body"},
+        {title:"title",field:"title"},
         // {
         //     title:"Name",field: ""
         // }
@@ -45,8 +59,8 @@ const TableListing=()=>{
             <h1>Catalog Listing page</h1>
             <MaterialTable 
             title="Catalog Table"
-            data={data}
-            columns={columns}
+            data={dataStatic}
+            columns={columnsStatic}
             options={{
                 search:true,
                 paging:true,
